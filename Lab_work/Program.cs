b61e3facb873991e5e95ad910f8515e1f249a433
ПК\ноутбук <djjamp64@gmail.com>
2019-04-23 13:51:39 +0300

    Добавьте файлы проекта.

---
+++ Lab_work/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Debug!");
+            Console.WriteLine("Hello Read!");
+            Console.ReadLine();
+        }
+    }
+}
